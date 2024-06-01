6e10dc03303a6fc3efc6d0c74a3c1fbe35bd8120
yamagame <yamagame@mac.com>
2024-06-01 10:14:11 +0900

    最初のコミット

---
+++ Makefile
@@ -0,0 +1,14 @@
+clean:
+	rm proto/school/*.go
+	rm proto/school/*.json
+
+generate:
+	buf generate --path ./proto/school/school.proto
+
+install:
+	go get \
+		github.com/bufbuild/buf/cmd/buf \
+		github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway \
+		github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2 \
+		google.golang.org/grpc/cmd/protoc-gen-go-grpc \
+		google.golang.org/protobuf/cmd/protoc-gen-go
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pbSchool "github.com/yamagame/school-api-gateway/proto/school"
+)
+
+// School サービスの構造体
+type server struct{}
+
+// ListLabos 研究室の一覧を返す
+func (*server) ListLabos(_ context.Context, in *pbSchool.ListLabosRequest) (*pbSchool.ListLabosResponse, error) {
+	pageSize := int32(5)
+	if in.PageSize != nil {
+		pageSize = *in.PageSize
+	}
+	labos := []*pbSchool.Labo{}
+	for i := int32(0); i < pageSize; i++ {
+		labos = append(labos, &pbSchool.Labo{
+			Name: fmt.Sprintf("研究室-%04d", i+1),
+		})
+	}
+	return &pbSchool.ListLabosResponse{Labos: labos, Offset: pageSize}, nil
+}
+
+func main() {
+	// TCP ポートを作成する
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("TCP ポートのリッスンに失敗:", err)
+	}
+
+	// gRPC サーバーオブジェクトを作成する
+	s := grpc.NewServer()
+	// School サービスを接続
+	pbSchool.RegisterShoolServer(s, &server{})
+	// gRPC サーバーを起動
+	log.Println("gRPC を起動 0.0.0.0:8080")
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+
+	// gRPC サーバーに接続するクライアントの作成
+	conn, err := grpc.NewClient(
+		"0.0.0.0:8080",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalln("gRPC クライアントの起動に失敗:", err)
+	}
+
+	// ServeMux の作成
+	gwmux := runtime.NewServeMux()
+	// School サービスを gRPC-Gateway に登録
+	err = pbSchool.RegisterShoolHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("サービスの登録に失敗:", err)
+	}
+
+	// HTTP サーバーを作成
+	gwServer := &http.Server{
+		Addr:    ":8090",
+		Handler: gwmux,
+	}
+
+	log.Println("gRPC-Gateway 起動 http://0.0.0.0:8090")
+	log.Fatalln(gwServer.ListenAndServe())
+}
