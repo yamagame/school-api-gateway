9931f88531718dab6616a20bde8eb352b5c6b107
yamagame <yamagame@mac.com>
2024-07-09 22:44:21 +0900

    csv json 変換ツール追加

--- cmd/csv2json/main.go
+++ cmd/csv2json/main.go
@@ -4,39 +4,138 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	"log"
+	"io"
 	"os"
+	"regexp"
+	"slices"
+	"strings"
 )
 
-func main() {
-	// CSVリーダーを作成
-	reader := csv.NewReader(os.Stdin)
-	records, err := reader.ReadAll()
-	if err != nil {
-		log.Fatal(err)
+func fillCSV(records [][]string) []map[string]string {
+	ret := []map[string]string{}
+	header := records[0]
+	prevalues := map[string]string{}
+	for _, record := range records[1:] {
+		field := map[string]string{}
+		for i, column := range header {
+			r := strings.TrimSpace(record[i])
+			if r == "-" {
+				continue
+			} else if r != "" {
+				prevalues[column] = r
+				field[column] = r
+			} else {
+				field[column] = prevalues[column]
+			}
+		}
+		ret = append(ret, field)
+	}
+	return ret
+}
+
+func isEqualStrings(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if b[i] != v {
+			return false
+		}
+	}
+	return true
+}
+
+func mergeRecords(records []map[string]interface{}) []map[string]interface{} {
+	ret := []map[string]interface{}{}
+
+	re1, _ := regexp.Compile(`(\..+\[\])(.*)`)
+	re2, _ := regexp.Compile(`(\..+)\[\](.*)`)
+
+	pkeys := []string{}
+	var r map[string]interface{}
+	for _, record := range records {
+		heads := []string{}
+
+		for k, v := range record {
+			matches := re1.FindAllStringSubmatch(k, -1)
+			if len(matches) > 0 {
+			} else {
+				heads = append(heads, v.(string))
+			}
+		}
+
+		slices.Sort(heads)
+		if !isEqualStrings(pkeys, heads) {
+			pkeys = heads
+			r = map[string]interface{}{}
+			ret = append(ret, r)
+		}
+
+		for k, v := range record {
+			matches := re2.FindAllStringSubmatch(k, -1)
+			if len(matches) > 0 {
+				head := matches[0][1]
+				var h []map[string]string
+				if _, ok := r[head]; !ok {
+					h = []map[string]string{}
+				} else {
+					h = r[head].([]map[string]string)
+				}
+				h = append(h, v.(map[string]string))
+				r[head] = h
+			} else {
+				r[k] = v
+			}
+		}
 	}
 
-	// CSVのヘッダーを取得
-	header := records[0]
+	return ret
+}
+
+func dumpCSV(in io.Reader) ([]byte, error) {
+	// CSVリーダーを作成
+	reader := csv.NewReader(in)
+	csvdata, err := reader.ReadAll()
+	if err != nil {
+		return nil, err
+	}
+	records := fillCSV(csvdata)
 
 	// JSONデータを格納するためのスライス
-	var jsonData []map[string]string
+	var jsondata []map[string]interface{}
+
+	re, _ := regexp.Compile(`(\..+\[\])(.*)`)
 
 	// 各レコードを処理
-	for _, record := range records[1:] {
-		item := make(map[string]string)
-		for i, v := range record {
-			item[header[i]] = v
+	for _, record := range records {
+		item := make(map[string]interface{})
+		for head, v := range record {
+			matches := re.FindAllStringSubmatch(head, -1)
+			if len(matches) > 0 {
+				head := matches[0][1]
+				val := matches[0][2]
+				if _, ok := item[head]; !ok {
+					item[head] = map[string]string{}
+				}
+				t := item[head].(map[string]string)
+				t[val] = v
+			} else {
+				item[head] = v
+			}
 		}
-		jsonData = append(jsonData, item)
+		jsondata = append(jsondata, item)
 	}
+
+	jsondata = mergeRecords(jsondata)
 
 	// JSONに変換
-	jsonBytes, err := json.MarshalIndent(jsonData, "", "    ")
-	if err != nil {
-		log.Fatal(err)
-	}
+	return json.MarshalIndent(jsondata, "", "    ")
+}
 
-	// 標準出力にJSONを出力
-	fmt.Println(string(jsonBytes))
+func main() {
+	v, err := dumpCSV(os.Stdin)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(v))
 }
---
+++ cmd/csv2json/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/yamagame/school-api-gateway/pkg/snapshot"
+)
+
+func TestCSV2JSON(t *testing.T) {
+	testdata := filepath.Join("testdata", "test-data.csv")
+	fp, err := os.Open(testdata)
+	assert.NoError(t, err)
+	v, err := dumpCSV(fp)
+	assert.NoError(t, err)
+
+	out := []map[string]interface{}{}
+	err = json.Unmarshal(v, &out)
+	assert.NoError(t, err)
+
+	snapshot.Equal(t, out, "test-data.json")
+	// snapshot.Save(t, out, "test-data.json")
+}
